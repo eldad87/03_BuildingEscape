55885f09c7329e5c7452e3bbd043df343fba5a0f
eldad87 <eldad87@gmail.com>
2017-05-12 08:16:28 -0700

    Door

--- BuildingEscape/Source/BuildingEscape/NewActorComponent.cpp
+++ BuildingEscape/Source/BuildingEscape/NewActorComponent.cpp
@@ -1,34 +1,33 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BuildingEscape.h"
-#include "PositionReport.h"
+#include "NewActorComponent.h"
 
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UNewActorComponent::UNewActorComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
 	// ...
+	UE_LOG(LogTemp, Warning, TEXT("New Ball Reporting for duty!"));
 }
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UNewActorComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
 	// ...
-	FString ObjectName = GetOwner()->GetName();
-	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
-	UE_LOG(LogTemp, Warning, TEXT("%s Reporting from %s"), *ObjectName, *ObjectPos);
+	
 }
 
 
 // Called every frame
-void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UNewActorComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
